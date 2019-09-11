fadaa1a8cccd9874f311d5c3957403f6cd37bec9
Matt <matt.miller041798@gmail.com>
2019-09-11 17:32:32 -0400

    Added enemy follow functionality

--- enemy.js
+++ enemy.js
@@ -10,19 +10,46 @@ function getPlayerPos(game){
     console.log("PLAYER POSx: " + player_x + " y:" + player_y);
     console.log("PLAYER MID POSx: " + mid_x + " y:" + mid_y);
 }
+function getPlayerX(game){
+    player_x = game.player.x;
+    mid_x =  player_x + (game.player.width / 2);
+    return mid_x;
+}
 
-function enemyComponent(width, height, x, y){
+function getPlayerY(game){
+    player_y = game.player.x;
+    mid_y =  player_y + (game.player.height / 2);
+    return mid_y;
+}
+
+function enemyComponent(width, height, x, y, game){
     var self = this;
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
+    this.moveInc = 2;
     this.x = x;
     this.y = y;
 
     this.update = function() {
         self.speedX = 0;
         self.speedY = 0;
-        //Move where the player is...
+        var player_x = getPlayerX(game);
+        var player_y = getPlayerY(game);
+        //Move where the player is ...
+        if(x < player_x){ self.speedX = 2; }         // Move right
+        else if(x > player_x){ self.speedX = -2; } // Move left
+        if(y < player_y){ self.speedY = 2; }         // Move down
+        else if(y > player_y){ self.speedY = -2; } // Move up
+
+        self.x += self.speedX;
+        self.y += self.speedY;
+
+        console.log(player_x + " " + player_y);
+
+        // Update the css to show the movement
+        $("#enemy").css("left",self.x);
+        $("#enemy").css("top",self.y);
     }
 }
--- index.js
+++ index.js
@@ -14,8 +14,8 @@ $(function(){
             game.initGame();
             // run update every 10 msec
             game.interval = setInterval(game.player.update, 10);
+            game.interval = setInterval(game.enemy.update, 10);     
             $("#gameScreen").fadeIn();
-            getPlayerPos(game);
         });
     });
 
@@ -47,12 +47,18 @@ $(function(){
 function gameInstance(){
     var self = this;
     this.player = undefined;
+    // keep below?
+
+    this.enemy = undefined;
+
+    // keep above?
     this.running = false;
     this.interval = undefined;
 
     this.initGame = function() {
         self.running = true;
         self.player = new gameComponent(50, 50, 400, 300);
+        self.enemy = new enemyComponent(75, 75, 200, 100);
     }
 }
 
@@ -82,5 +88,65 @@ function gameComponent(width, height, x, y) {
 
         $("#player").css("left",self.x);
         $("#player").css("top",self.y);
+    }
+}
+
+// Enemy code below this
+
+function getPlayerX(){
+    player_x = game.player.x;
+    mid_x =  player_x + (game.player.width / 2);
+    return mid_x;
+}
+
+function getPlayerY(){
+    player_y = game.player.y;
+    mid_y =  player_y + (game.player.height / 2);
+    return mid_y;
+}
+
+function enemyComponent(width, height, x, y){
+    var self = this;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.moveInc = 2;
+    this.x = x;
+    this.y = y;
+
+    this.update = function() {
+        self.speedX = 0;
+        self.speedY = 0;
+        var player_x = getPlayerX();
+        var player_y = getPlayerY();
+        var left = false, right = false, up = false, down = false; // Set booleans
+
+        //Try manhatten distance ... 
+        var x_mid = self.x + self.width/2;
+        var y_mid = self.y + self.height/2;
+        var x_distance = x_mid - player_x;
+        var y_distance = y_mid - player_y;
+
+        if(Math.abs(x_distance) >= Math.abs(y_distance)){
+            // Move x direction
+            if(x_mid > player_x) { self.speedX = 0 - self.moveInc; } // Move left
+            else if(x_mid < player_x) { self.speedX = self.moveInc; } // Move right
+            // Check the bounds
+            if (self.x + self.speedX >= 0 && self.x + self.speedX + self.height <= 800)          
+                self.x += self.speedX;
+        }
+        else{
+            // Move y direction
+            if(y_mid > player_y) { self.speedY = 0 - self.moveInc; } // Move down
+            else if(y_mid < player_y) { self.speedY = self.moveInc; } // Move up
+            // Check the bounds
+            if (self.y + self.speedY >= 0 && self.y + self.speedY + self.height <= 600)
+                self.y += self.speedY;
+        }
+         
+        // Update the css to show the movement
+        $("#enemy").css("left",self.x);
+        $("#enemy").css("top",self.y);
     }
 }
